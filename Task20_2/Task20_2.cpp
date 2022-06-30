ebb215ce7c14b3a9936851c85d0e76f737cc47f9
DmityFilchev <filchev_dmitry@mail.ru>
2022-06-30 16:46:00 +0300

    Добавьте файлы проекта.

---
+++ Task20_2/Task20_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<fstream>
+#include<ctime>
+
+int main()
+{
+	std::srand(std::time(nullptr));
+	int width = 30;
+	int height = 30;
+	std::cout << "Enter the width : ";
+	std::cin >> width;
+	std::cout << "Enter the height : ";
+	std::cin >> height;
+	std::ofstream pic("pic.txt");
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			pic << rand()%2;
+		}
+		pic << std::endl;
+	}
+}
